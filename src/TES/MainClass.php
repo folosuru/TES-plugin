c83f7bbb17e4488db413c209afd2701bc2478041
folosuru <77926718+folosuru@users.noreply.github.com>
2021-03-30 18:14:45 +0900

    Add files via upload

--- src/TES/MainClass.php
+++ src/TES/MainClass.php
@@ -52,18 +52,20 @@ class MainClass extends PluginBase implements Listener{
 	}
 
 	public function onEnable() : void{
-		$player_data = [];
-		$player_data_add_count = 0;
+		$this->someword = "unchi";
+		$this->country =  array('zennaka' => , );
 		$this->getServer()->getPluginManager()->registerEvents(new ExampleListener($this), $this);
 		$this->getScheduler()->scheduleRepeatingTask(new BroadcastTask($this->getServer()), 1200);//BroadcastTaskのアレを120tickごとに動かす
+		//$this->getScheduler()->scheduleRepeatingTask(new InfoBarTask($this->getServer()), 4);
 		$this->getLogger()->info(TextFormat::DARK_GREEN . "I've been enabled!");
 //		$content = new Content();
 //		$content->setText("サーバーが起動しました");
-//		$webhook = Sender::create("https://discord.com/api/webhooks/816533633208549376/CyIo52uELVMlPrYjwD2xLzLOf5HXblG9UjNNFSmUbvXgiyCZFIwkzhZOTs-0JkRdNq_5")
+//		$webhook = Sender::create($this->WebhookURL)
 //			->add($content)
 //			->setCustomName("from PMMP server");
 //		Sender::send($webhook);
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->WebhookURL = "https://discord.com/api/webhooks/816533633208549376/CyIo52uELVMlPrYjwD2xLzLOf5HXblG9UjNNFSmUbvXgiyCZFIwkzhZOTs-0JkRdNq_5";
 	}
 
 	public function onDisable() : void{
@@ -84,7 +86,7 @@ class MainClass extends PluginBase implements Listener{
 			case "disc":
 				$content = new Content();
 				$content->setText("This message was sent from PMMP server.Hello,discord.py.");
-				$webhook = Sender::create("https://discord.com/api/webhooks/816533633208549376/CyIo52uELVMlPrYjwD2xLzLOf5HXblG9UjNNFSmUbvXgiyCZFIwkzhZOTs-0JkRdNq_5")
+				$webhook = Sender::create($this->WebhookURL)
 					->add($content)
 					->setCustomName("from PMMP server");
 				Sender::send($webhook);
@@ -94,10 +96,35 @@ class MainClass extends PluginBase implements Listener{
 				return true;
 			case "savepda":
 				$arr = json_encode($this->player_data[$sender->getName()]);
-				file_put_contents("plugin_data/TES/".$sender->getName().".json",$arr);
+				file_put_contents("plugin_data/TES/player/".$sender->getName().".json",$arr);
+				return true;
+			case "country":
+				if (empty($args)){
+					$sender->sendMessage("使い方は/country helpで確認してください。");
+				}else {
+					switch ($args[0]) {
+						case 'help':
+							$sender->sendMessage("[country help]");
+							$sender->sendMessage("/country help:このヘルプを表示します。");
+							$sender->sendMessage("/country new [名前]:新たに国を作成します。すでに国に所属している場合はできません。");
+							return true;
+						case "new":
+							$sender->sendMessage($args[0].$args[1]);
+							/*if ($this->player_data[$sender->getName()]["country"] = "default"){
+								if (array_key_exists($args[1],$this->country_data)){
+									$sender->sendMessage("[国家作成]その名前の国家はすでに存在します");
+								}else {
+									$sender->sendMessage("新たに国家".$args[1]."を作成しました！");
+								}
+							}*/
+						default:
+							// code...
+							break;
+					}				}
 				return true;
 			default:
 				throw new \AssertionError("This line will never be executed");
+
 		}
 	}
 	public function onChat(PlayerChatEvent $event){
@@ -105,7 +132,7 @@ class MainClass extends PluginBase implements Listener{
 		$player = $event->getPlayer();
 		$message = $event->getMessage();
 		$content->setText("<" .$player->getname().">"." ".$message);
-		$webhook = Sender::create("https://discord.com/api/webhooks/816533633208549376/CyIo52uELVMlPrYjwD2xLzLOf5HXblG9UjNNFSmUbvXgiyCZFIwkzhZOTs-0JkRdNq_5")
+		$webhook = Sender::create($this->WebhookURL)
 			->add($content)
 			->setCustomName("マイクラ内チャット");
 		Sender::send($webhook);
@@ -113,18 +140,18 @@ class MainClass extends PluginBase implements Listener{
 	public function onLogin(PlayerLoginEvent $event){
 		#$content = new Content();
 		#$content->setText("Player login! Name:".$event->getPlayer()->getName()." ID:".$event->getPlayer()->getXuid());//EventからPlayerを取得、そこからXuid取得
-		#$webhook = Sender::create("https://discord.com/api/webhooks/816533633208549376/CyIo52uELVMlPrYjwD2xLzLOf5HXblG9UjNNFSmUbvXgiyCZFIwkzhZOTs-0JkRdNq_5")
+		#$webhook = Sender::create($this->WebhookURL)
 		#	->add($content)
 		#	->setCustomName("test");
 		#Sender::send($webhook);
-		if (file_exists("plugin_data/TES/".$event->getPlayer()->getName().".json")){
-			$player_data_tmp = file_get_contents("plugin_data/TES/".$event->getPlayer()->getName().".json");
+		if (file_exists("plugin_data/TES/player/".$event->getPlayer()->getName().".json")){
+			$player_data_tmp = file_get_contents("plugin_data/TES/player/".$event->getPlayer()->getName().".json");
 			#$player_data_tmp = mb_convert_encoding($player_data_tmp,"UTF8",auto);
 			$this->player_data[$event->getPlayer()->getName()] = $player_data_tmp;
 			$event->getPlayer()->sendMessage($this->player_data[$event->getPlayer()->getName()]);
 			$event->getPlayer()->sendMessage("おかえりなさい、".$event->getPlayer()->getName()."さん！");
 		}else {
-			$this->player_data[$event->getPlayer()->getName()] = array('name' => $event->getPlayer()->getName() , "first_login" => 1 ,"ACP" => 0 );
+			$this->player_data[$event->getPlayer()->getName()] = array('name' => $event->getPlayer()->getName() , "first_login" => 1 ,"ACP" => 0,"country" =>"default" );
 			$this->getLogger()->info($this->player_data[$event->getPlayer()->getName()]["name"]);
 			$this->getLogger()->info("プレイヤーデータが存在しないため、新規作成しました。")	;
 		}
@@ -140,5 +167,7 @@ class MainClass extends PluginBase implements Listener{
 	}
 	public function onQuit(PlayerQuitEvent $event){
 		$this->getLogger()->info(TextFormat::WHITE . "Player Quit. Name:".$event->getPlayer()->getName());
+		$arr = json_encode($this->player_data[$event->getPlayer()->getName()]);
+		file_put_contents("plugin_data/TES/player/".$event->getPlayer()->getName().".json",$arr);
 	}
 }
